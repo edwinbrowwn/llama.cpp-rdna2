a847860eafdc5906c11c7be0afcce50179b4d18a
edwinbrowwn <edwin.dev@hotmail.com>
2026-08-01 05:08:28 +0000

    fix: sample DSpark logits with target vocabulary

--- common/speculative.cpp
+++ common/speculative.cpp
@@ -999,7 +999,7 @@ struct common_speculative_impl_draft_dflash : public common_speculative_impl {
             sparams.no_perf  = false;
             sparams.top_k    = 10;
             sparams.samplers = { COMMON_SAMPLER_TYPE_TOP_K };
-            s.reset(common_sampler_init(model_dft, sparams));
+            s.reset(common_sampler_init(is_dspark ? model_tgt : model_dft, sparams));
         }
 
         // turn on extraction of the target layers' input embeddings; an id equal
